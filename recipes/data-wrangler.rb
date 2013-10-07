7270ed26a92b8ca6d67c3ffcc5a3e6c67615280a
shunwen <hsiaoshunwen@gmail.com>
2013-10-07 12:19:22 +0800

    Use #{env} for RAILS_ENV

--- recipes/data-wrangler.rb
+++ recipes/data-wrangler.rb
@@ -7,5 +7,5 @@ cron_d "update_in_period" do
   user "devel"
   path "/usr/local/rbenv/shims:/usr/local/bin:/usr/bin:/bin:$PATH"
   minute "01"
-  command "cd /var/www/datawrangler;RACK_ENV=#{node[:datawrangler][:rack_env]} bundle exec rake update_in_period period=hourly"
+  command "cd /var/www/datawrangler;RACK_ENV=#{env} bundle exec rake update_in_period period=hourly"
 end
