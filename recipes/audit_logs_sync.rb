53ec8c6da8d95eb7f372637fc406d83360db4d18
Umang Chouhan <uchouhan@optimiscorp.com>
2013-12-26 14:39:39 +0530

    Fixing hour and minute for audit logs sync.

--- recipes/audit_logs_sync.rb
+++ recipes/audit_logs_sync.rb
@@ -1,7 +1,7 @@
 include_recipe 'cron::default'
 
 cron 'audit_logs_sync' do
-  minute '00'
-  hour '30'
+  minute '30'
+  hour '00'
   command "s3cmd sync /var/log/td-agent/log_entries.raw_data.*.gz s3://audit-logs-#{node.chef_environment}/archive"
 end
