056648cb2ae1dca0daee4ef94d6cf88eda04880f
Umang Chouhan <uchouhan@optimiscorp.com>
2013-12-26 14:27:58 +0530

    Adding recipe for audit logs sync.

---
+++ recipes/audit_logs_sync.rb
@@ -0,0 +1,7 @@
+include_recipe 'cron::default'
+
+cron 'audit_logs_sync' do
+  minute '00'
+  hour '30'
+  command "s3cmd sync /var/log/td-agent/log_entries.raw_data.*.gz s3://audit-logs-#{node.chef_environment}"
+end
