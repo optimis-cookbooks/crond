1a157059ad3b4a0f3bd7b6fcc585d225b1990e2c
Umang Chouhan <uchouhan@optimiscorp.com>
2013-12-26 14:35:52 +0530

    Fixing archive directory.

--- recipes/audit_logs_sync.rb
+++ recipes/audit_logs_sync.rb
@@ -3,5 +3,5 @@ include_recipe 'cron::default'
 cron 'audit_logs_sync' do
   minute '00'
   hour '30'
-  command "s3cmd sync /var/log/td-agent/log_entries.raw_data.*.gz s3://audit-logs-#{node.chef_environment}"
+  command "s3cmd sync /var/log/td-agent/log_entries.raw_data.*.gz s3://audit-logs-#{node.chef_environment}/archive"
 end
