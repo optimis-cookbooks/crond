318a25baaf372c94b0e907020d563aa2ca3c5a9d
Umang Chouhan <uchouhan@optimiscorp.com>
2013-12-26 15:09:17 +0530

    Adding cronjob to purge audit logs after 90 days.

--- recipes/audit_logs_sync.rb
+++ recipes/audit_logs_sync.rb
@@ -5,3 +5,9 @@ cron 'audit_logs_sync' do
   hour '00'
   command "s3cmd sync /var/log/td-agent/log_entries.raw_data.*.gz s3://audit-logs-#{node.chef_environment}/archive"
 end
+
+cron 'audit_logs_purge' do
+  minute '00'
+  hour '01'
+  command 'find /var/logs/td-agent/log_entries.*.gz -mtime +90 -exec rm {} \;'
+end
