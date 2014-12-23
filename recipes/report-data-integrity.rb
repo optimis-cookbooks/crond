3bb34dd312e8ae60f7e6906f6cd776d064165a96
sydchen <schen@optimisdev.com>
2014-12-23 16:26:50 +0800

    remove old reporting jobs

--- recipes/report-data-integrity.rb
+++ recipes/report-data-integrity.rb
@@ -5,22 +5,6 @@ include_recipe "crond::default"
 env = node.chef_environment.downcase
 env = 'production' if env == 'prod'
 
-cron_d "check_id" do
-  user "devel"
-  path "/usr/local/rbenv/shims:/usr/local/bin:/usr/bin:/bin:$PATH"
-  minute "20"
-  hour "23"
-  command "cd /var/www/optimis;RAILS_ENV=#{env} bundle exec rake data_integrity:check_ids auto_update=true > /var/www/optimis/log/reporting_check_ids_$(date +\\%m\\%d\\%Y).log 2>&1"
-end
-
-cron_d "check_data" do
-  user "devel"
-  path "/usr/local/rbenv/shims:/usr/local/bin:/usr/bin:/bin:$PATH"
-  minute "0"
-  hour "0"
-  command "cd /var/www/optimis;RAILS_ENV=#{env} bundle exec rake data_integrity:check_data auto_update=true from_date=$(date -dyesterday +\\%m/\\%d/\\%Y) > /var/www/optimis/log/reporting_check_data_$(date -dyesterday +\\%m\\%d\\%Y).log 2>&1"
-end
-
 cron_d "mq_sync_data" do
   user "devel"
   path "/usr/local/rbenv/shims:/usr/local/bin:/usr/bin:/bin:$PATH"
