84509ec4e504a8a61e918a8012ca9fa8c6190bbc
sydchen <schen@optimisdev.com>
2014-12-23 16:29:44 +0800

    Bumped version to 0.0.7

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures cron"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.6"
+version          "0.0.7"
 depends          "cron"
