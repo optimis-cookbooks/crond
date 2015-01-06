e0af8a239eb61df393a8beae68e9a571b82d4920
Syd Chen <schen@optimisdev.com>
2015-01-06 16:13:19 +0800

    remove reporting cron job

--- recipes/reporting.rb
+++ recipes/reporting.rb
@@ -13,14 +13,6 @@ cron_d "mailchimp" do
   command "cd /var/www/reporting;RAILS_ENV=#{env} bundle exec rake export:mailchimp:run"
 end
 
-cron_d "remove_duplicated_documents" do
-  user "devel"
-  path "/usr/local/rbenv/shims:/usr/local/bin:/usr/bin:/bin:$PATH"
-  minute '0'
-  hour '2'
-  command "cd /var/www/reporting;RAILS_ENV=#{env} bundle exec rake duplicates:remove"
-end
-
 cron_d "denormalizer_periodly_update" do
   user "devel"
   path "/usr/local/rbenv/shims:/usr/local/bin:/usr/bin:/bin:$PATH"
